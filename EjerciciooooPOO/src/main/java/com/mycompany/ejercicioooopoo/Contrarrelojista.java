9f5f0802b9c1851f2bc7b65e4206b6ab732f09df
malec01 <161643726+malec01@users.noreply.github.com>
2024-08-18 12:25:06 -0500

    Add files via upload

---
+++ EjerciciooooPOO/src/main/java/com/mycompany/ejercicioooopoo/Contrarrelojista.java
@@ -0,0 +1,25 @@
+
+package com.mycompany.ejercicioooopoo;
+public class Contrarrelojista extends Ciclista{
+    private double velocidadMaxima;
+    
+    public Contrarrelojista(int identificador, String nombre, double velocidadMaxima){
+        super(identificador,nombre);
+        this.velocidadMaxima = velocidadMaxima;
+    }
+    
+    protected double getVelocidadMaxima(){
+        return velocidadMaxima;
+    }
+    protected void setVelocidadMaxima(double velocidadMaxima){
+        this.velocidadMaxima = velocidadMaxima;
+    }
+    protected void imprimir(){
+        super.imprimir();
+        System.out.println("Aceleración promedio =" + velocidadMaxima);
+    }
+    
+    protected String imprimirTipo(){
+        return "Es un contrarrelojista";
+    }
+}
---
+++ EjerciciooooPOO/src/main/java/com/mycompany/ejercicioooopoo/Velocista.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicioooopoo;
+
+/**
+ *
+ * @author Alejandra Castaño
+ */
+public class Velocista extends Ciclista {
+    
+    private double potenciaPromedio;
+    private double velocidadPromedio;
+    
+    public Velocista(int identificador, String nombre, double potenciaPromedio, double velocidadPromedio){
+        super(identificador, nombre);
+        potenciaPromedio = potenciaPromedio;
+        this.velocidadPromedio=velocidadPromedio;
+    }
+    
+    protected double getPotenciaPromedio(){
+        return potenciaPromedio;
+    }    
+    
+    protected void setPotenciaPromedio(double potenciPromedio){
+        this.potenciaPromedio = potenciaPromedio;
+    }
+    protected double getVelocidadPromedio(){
+        return velocidadPromedio;
+    }
+    protected void setVelocidadPromedio(double velocidadPromedio){
+        this.velocidadPromedio = velocidadPromedio;
+    }
+    
+    protected void imprimir(){
+        super.imprimir();
+        System.out.println("Potencia promedio= " + potenciaPromedio);
+        System.out.println("Velocidad promedio= "+ velocidadPromedio);
+    }
+    
+    protected String imprimirTipo(){
+        return "Es un velocista";
+    }
+    
+    
+}
